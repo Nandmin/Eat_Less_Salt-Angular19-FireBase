9c4b5be64a65d204d4a59dcdd23937621569ce87
Nandmin <andras78.nemeth@gmail.com>
2024-12-25 01:20:09 +0100

    Some changes in basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+// import { ItemsComponent } from "./pages/items/items.component";
+// import { HomeComponent } from "./pages/home/home.component";
+// import { LoginComponent } from "./pages/login/login.component";
+// import { InformationComponent } from "./pages/information/information.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Eat_Less_Salt-Angular19-FireBase';
+  title = 'Fogyassz kevesebb sót - reloaded';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ItemsComponent } from './pages/items/items.component';
+import { InformationComponent } from './pages/information/information.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', component: HomeComponent, redirectTo: 'home', pathMatch: 'full' },
+    { path: '', component: HomeComponent},
+    // { path: '/items', component: ItemsComponent, redirectTo: '/items', pathMatch: 'full' },
+    { path: 'items', component: ItemsComponent  },
+    // { path: '/informations', component: InformationComponent, redirectTo: 'informations', pathMatch: 'full' },
+    { path: 'informations', component: InformationComponent }
+];
