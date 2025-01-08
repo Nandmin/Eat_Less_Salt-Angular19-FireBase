e465ba29f8b7d4ed17b4e62217ddad27341bca5a
Nandmin <andras78.nemeth@gmail.com>
2025-01-08 01:11:14 +0100

    Items added

---
+++ src/app/model/items.ts
@@ -0,0 +1,13 @@
+export class Items {
+    docID?: string;
+    id!: number;
+    name!: string;
+    energyKJ!: number;
+    energyKcal!: number;
+    fat!: number;
+    sFat!: number;
+    carbonHydrate!: number;
+    sugar!: number;
+    protein!: number;
+    salt!: number;
+}
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 // bootstrapApplication(AppComponent, appConfig)
 //   .catch((err) => console.error(err));
 
   bootstrapApplication(AppComponent, {
     providers: [
-      provideRouter(routes, withComponentInputBinding())
+      provideRouter(routes, withComponentInputBinding()), provideAnimationsAsync()
     ]
   }).catch(err => console.error(err));
